cd8fc06c80f4afb043290569debbc5ddb9f53163
chandureddy <csreddy.512@gmail.com>
2015-03-24 23:41:39 -0500

    changes made to footer

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <style type="text/css">
     footer {
             color: #666;
-            background: #222;
+          
             padding: 17px 0 18px 0;
             border-top: 1px solid #000;
             bottom:0px;
@@ -41,12 +41,12 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="#" class="navbar-brand">Brand</a>
+            <a href="#" class="navbar-brand">Chandu</a>
         </div>
         <!-- Collection of nav links and other content for toggling -->
         <div id="navbarCollapse" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
                 
                 
             </ul>
@@ -60,7 +60,7 @@
 
 <footer>
       <div class="container">
-        <p class="footerp">Put together in less than five minutes by <a href="http://chandrasekharreddy.me/" rel="author">Chandrasekhar</a>. Uses <a href="http://twitter.github.com/bootstrap/" rel="external">Twitter Bootstrap</a> </p>
+        <p class="footerp">All Copy Rights are Reserved<a href="http://chandrasekharreddy.me/" rel="author">Chandrasekhar</a>. Uses <a href="http://twitter.github.com/bootstrap/" rel="external">Twitter Bootstrap</a> </p>
       </div>
     </footer>
 </body>
